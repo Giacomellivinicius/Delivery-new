800bfe82cf033d86fa15541f12bbd8768d913b9c
Marthiins <sergio.marthiins@gmail.com>
2021-02-01 16:40:54 -0300

    Projeto criado

---
+++ backend/src/main/java/com/delivery/deliverynew/DeliveryNewApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.deliverynew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryNewApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/delivery/deliverynew/DeliveryNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.deliverynew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
